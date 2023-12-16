08ad6a463b7e5472991df4b70256ede2e7150031
Nikita Skripka <>
2023-12-17 00:27:32 +1000

    Added InformationTaskViewController, the project was sorted for a convenient navigation.

--- TestProject/TestProject/Controller/FirstTableViewController.swift
+++ TestProject/TestProject/Controller/FirstTableViewController.swift
@@ -9,8 +9,10 @@ import UIKit
 
 class FirstTableViewController: UITableViewController {
 
+    //MARK: - properties
     var tasks = [Task]()
     
+    //MARK: - View life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
      
@@ -22,6 +24,10 @@ class FirstTableViewController: UITableViewController {
         tableView.register(nibTaskCell, forCellReuseIdentifier: "TaskCell")
     }
 
+}
+
+//MARK: - TableViewDelegate and TableVIewDataSource
+extension FirstTableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         tasks.count
     }
@@ -37,6 +43,13 @@ class FirstTableViewController: UITableViewController {
         return cell
     }
     
-
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(identifier: "TaskInformationViewController")
+        
+        vc.view.backgroundColor = .gray
+        
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
 }
-
---
+++ TestProject/TestProject/Controller/TaskInformationViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TaskInformationViewController.swift
+//  TestProject
+//
+//  Created by Nikita Skripka on 17.12.2023.
+//
+
+import UIKit
+
+class TaskInformationViewController: UIViewController {
+
+    //MARK: - View life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
